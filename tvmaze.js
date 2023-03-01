d915eb1d2a168e4eba5dffc0bd0c8dbd3c0ba575
Kevin Nguyen <kayvinhnguyen@gmail.com>
2023-03-01 13:10:40 -0800

    Finished up to step 4

--- tvmaze.js
+++ tvmaze.js
@@ -3,7 +3,11 @@
 const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
+const $searchFormInput = $("#searchForm-term");
+const BASE_URL = "https://api.tvmaze.com/";
 
+// clear placeholder
+$searchFormInput.val("");
 
 /** Given a search term, search for tv shows that match that query.
  *
@@ -12,27 +16,60 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm( /* term */) {
+async function getShowsByTerm(searchFormInput) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary 
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their 
-           normal lives, modestly setting aside the part they played in 
-           producing crucial intelligence, which helped the Allies to victory 
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She 
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+  // let inputValue = $searchFormInput.val();
+
+  // https://www.tvmaze.com/search/shows?q=:searchFormInput
+
+  const response = await axios.get(
+    `${BASE_URL}search/shows`,
+    { params: { q: searchFormInput } }
+  );
+
+  console.log(response.data);
+
+  const listOfShows = response.data.map((show) => {
+    return {
+      id: show.show.id,
+      name: show.show.name,
+      summary: show.show.summary,
+      image: show.show.image,
     }
-  ]
+  });
+
+  console.log("List of shows: " ,listOfShows);
+  return listOfShows;
+
+
+  // return [
+  //   {
+  //     id: ,
+  //     name: ,
+  //     summary: ,
+  //     image: 
+  //   }
+  // ]
+
+  // return [
+  //   {
+  //     id: 1767,
+  //     name: "The Bletchley Circle",
+  //     summary:
+  //       `<p><b>The Bletchley Circle</b> follows the journey of four ordinary 
+  //          women with extraordinary skills that helped to end World War II.</p>
+  //        <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their 
+  //          normal lives, modestly setting aside the part they played in 
+  //          producing crucial intelligence, which helped the Allies to victory 
+  //          and shortened the war. When Susan discovers a hidden code behind an
+  //          unsolved murder she is met by skepticism from the police. She 
+  //          quickly realises she can only begin to crack the murders and bring
+  //          the culprit to justice with her former friends.</p>`,
+  //     image:
+  //         "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+  //   }
+  // ]
 }
 
 
@@ -43,10 +80,12 @@ function populateShows(shows) {
 
   for (let show of shows) {
     const $show = $(
-        `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+      `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img 
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg" 
+              srcset="${show.image.original},
+              https://store-images.s-microsoft.com/image/apps.65316.13510798887490672.6e1ebb25-96c8-4504-b714-1f7cbca3c5ad.f9514a23-1eb8-4916-a18e-99b1a9817d15?mode=scale&q=90&h=300&w=300
+              " 
               alt="Bletchly Circle San Francisco" 
               class="w-25 me-3">
            <div class="media-body">
@@ -60,7 +99,8 @@ function populateShows(shows) {
        </div>
       `);
 
-    $showsList.append($show);  }
+    $showsList.append($show);
+  }
 }
 
 
